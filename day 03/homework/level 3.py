b7899aa88a52a0566776542683f90abaa803b41a
sabit0zz <email: sabakhidasheli2009@gmail.com>
2024-07-25 16:26:28 -0200

    second commit

---
+++ day 03/homework/level 3.py
@@ -0,0 +1,10 @@
+name = "saba"
+print(name)
+age = "14"
+print(age)
+time = "17"
+hour = "2"
+print(time+hour)
+truth = "W"
+TheBest = " GOA"
+print(truth+TheBest)
---
+++ day 04/homework/lesson 4.py
@@ -0,0 +1,22 @@
+age_and_height = "five and one hundred and sixty"
+print(age_and_height)
+# ცვლადი სახელით 'age and heigth', რომელიც შეიცავს სტრინგს
+# ცვლადი 'age and height'' ეხლა შეიცავს სტრინგს, ამიტომ მას გამოიტანეთ
+# კომენტარების განმარტება:
+# "age and height''' არის ცვლადი, რომლის სახელიც მოიცავს ორი სიტყვას, გაწვდილი ქვედა ხაზით (snake_case).
+# ცვლადში შეინახეთ სტრინგი: " five one hundred and sixty"
+# 'print()' ფუნქცია გამოაქვს ცვლადის მნიშვნელობა კონსოლზე, რაც ნიშნავს რომ სტრინგი გამოიტანება ეკრანზე.
+print(9 + 10)
+print(9 + 12)
+print(4 + 7)
+print(8 + 8)
+print(40 + 4)
+weight = 777
+score = 875
+print(weight + score)
+print(weight - score)
+print(weight * score)
+print(weight / score)
+#1N= ბ
+#2N="Case sensitive" ენა ნიშნავს, რომ ეს ენა უყურებს დიდ და პატარა ასოებს განსხვავებულად. ანუ, იმავე სიტყვის დაწერა სხვადასხვა რეგისტრში (მაგალითად, "Hello" და "hello") განსხვავებულ მნიშვნელობას ან შედეგებს შეიძლება გამოიწვიოს.
+#N3=კომენტარი არის ერთერთი
